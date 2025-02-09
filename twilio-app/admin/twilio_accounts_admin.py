8b628f7a4803bde927f9f4c5495c31c5ab231f40
Yogi Patel <yogipatel5@gmail.com>
2025-02-09 16:07:32 -0500

    fix imports

--- twilio-app/admin/twilio_accounts_admin.py
+++ twilio-app/admin/twilio_accounts_admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.utils.html import format_html
-from twilio.models import TwilioAccount
+
+from twilio_app.models import TwilioAccount
 
 
 @admin.register(TwilioAccount)
