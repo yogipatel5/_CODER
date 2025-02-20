5e14c5ae7a31b3ec8ed0f84ee25ada060457df2e
Yogi Patel <yogipatel5@gmail.com>
2025-02-19 20:29:53 -0500

    fixed adwords model

--- adwords/models/__init__.py
+++ adwords/models/__init__.py
@@ -1,6 +1,8 @@
 # adwords/models/__init__.py
 """Models for adwords app."""
 
+from django.db import models
+
 from shared.models import SharedTask, SharedTaskError
 
 from .manager_model import ManagerAccount
@@ -20,12 +22,9 @@ class Task(SharedTask):
 
 
 class TaskError(SharedTaskError):
-    """Model for tracking errors in Celery tasks in the adwords app."""
+    """Task error model for adwords app."""
 
-    class Meta(SharedTaskError.Meta):
-        app_label = "adwords"
-        verbose_name = "Task Error"
-        verbose_name_plural = "Task Errors"
+    task = models.ForeignKey("adwords.Task", on_delete=models.CASCADE, related_name="errors")
 
 
 __all__ = [
@@ -33,3 +32,39 @@ __all__ = [
     "TaskError",
     "ManagerAccount",
 ]
+
+
+# # adwords/models/__init__.py
+# """Models for pfsense app."""
+
+# from django.db import models
+
+# from shared.models import SharedTask, SharedTaskError
+
+# from .dhcproute import DHCPRoute
+
+
+# class Task(SharedTask):
+#     """Model for managing Celery tasks in the pfsense app."""
+
+#     class Meta(SharedTask.Meta):
+#         app_label = "pfsense"
+#         verbose_name = "Task"
+#         verbose_name_plural = "Tasks"
+#         ordering = ["name"]
+
+#     def __str__(self):
+#         return self.name
+
+
+# class TaskError(SharedTaskError):
+#     """Task error model for pfsense app."""
+
+#     task = models.ForeignKey("pfsense.Task", on_delete=models.CASCADE, related_name="errors")
+
+
+# __all__ = [
+#     "Task",
+#     "TaskError",
+#     "DHCPRoute",
+# ]
